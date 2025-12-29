7fbf8b80cd7c8226d613d4ae4ae26e1031886c8b
Yman14 <ymanneil07@gmail.com>
2025-12-29 18:04:38 +0800

    create library array to store all books and books object constructor and the book push to the array

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 document.querySelector("h1").textContent = "Welcome to the Library";
+
+const myLibrary = [];
+
+function Book(title, author, year, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.pages = pages || "Unknown";
+    this.status = status || "not-read";
+    this.uuid = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, year, pages, status) {
+    const newBook = new Book(title, author, year, pages, status);
+    myLibrary.push(newBook);
+}
+// Sample books
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 1925, 180,"read");
+addBookToLibrary("1984", "George Orwell", 1949);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 1960);
+
+console.table(myLibrary);
